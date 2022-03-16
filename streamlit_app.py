072174be1a4203c22a38cae87849783adba528d7
Shashank Goel <101084285+simplexgoel@users.noreply.github.com>
2022-03-15 21:19:54 -0700

    added balloons

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,6 +47,7 @@ widgetStr = """
 """
 # bootstrap 4 collapse example
 components.html(widgetStr, height=600,    )
+st.ballons()
 #with st.echo(code_location='below'):
 total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
 num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
